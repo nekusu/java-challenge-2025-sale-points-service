188a059075343334959bc1ea30110c6b02658d9e
nekusu <nekusu.dev@gmail.com>
2025-04-09 13:15:19 -0300

    Initialize sale points service with Spring Boot

---
+++ src/main/java/com/sharks/sale_points_service/SalePointsServiceApplication.java
@@ -0,0 +1,17 @@
+package com.sharks.sale_points_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+@SpringBootApplication
+public class SalePointsServiceApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+		SpringApplication.run(SalePointsServiceApplication.class, args);
+	}
+
+}
