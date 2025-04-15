ddfce1580987ff470a9ea81f472d00a7e1f5e157
nekusu <nekusu.dev@gmail.com>
2025-04-15 15:06:28 -0300

    Remove dotenv dependency

--- src/main/java/com/sharks/sale_points_service/SalePointsServiceApplication.java
+++ src/main/java/com/sharks/sale_points_service/SalePointsServiceApplication.java
@@ -4,15 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 @EnableCaching
 public class SalePointsServiceApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().load();
-		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
 		SpringApplication.run(SalePointsServiceApplication.class, args);
 	}
 
